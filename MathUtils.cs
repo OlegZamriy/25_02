ffd9b1ac9e44ce26ac524ac2a544045e313672e3
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 19:57:44 +0300

    1

---
+++ MathUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    public class MathUtils
+    {
+        public static int GetProductInRange(int start, int end)
+        {
+            int product = 1;
+            for (int i = start; i <= end; i++)
+            {
+                product *= i;
+            }
+            return product;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using _25_02;
+
+class Program
+{
+    static void Main()
+    {
+        int start = 1;
+        int end = 5;
+
+        int product = MathUtils.GetProductInRange(start, end);
+        Console.WriteLine($"Добуток чисел від {start} до {end} дорівнює {product}");
+    }
+}
