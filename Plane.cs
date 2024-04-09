8d5cc420465a22e4707f621bf8f07ba661729eda
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:04:33 +0300

    1

---
+++ Plane.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    using System;
+
+    public class Plane
+    {
+        private string planeName;
+        private string manufacturer;
+        private int yearOfManufacture;
+        private string planeType;
+
+        public Plane()
+        {
+            planeName = "";
+            manufacturer = "";
+            yearOfManufacture = 0;
+            planeType = "";
+        }
+
+        public Plane(string name, string manufacturer, int year, string type)
+        {
+            planeName = name;
+            this.manufacturer = manufacturer;
+            yearOfManufacture = year;
+            planeType = type;
+        }
+
+        public void SetPlaneName(string name)
+        {
+            planeName = name;
+        }
+
+        public string GetPlaneName()
+        {
+            return planeName;
+        }
+
+        public void SetManufacturer(string manufacturer)
+        {
+            this.manufacturer = manufacturer;
+        }
+
+        public string GetManufacturer()
+        {
+            return manufacturer;
+        }
+
+        public void SetYearOfManufacture(int year)
+        {
+            yearOfManufacture = year;
+        }
+
+        public int GetYearOfManufacture()
+        {
+            return yearOfManufacture;
+        }
+
+        public void SetPlaneType(string type)
+        {
+            planeType = type;
+        }
+
+        public string GetPlaneType()
+        {
+            return planeType;
+        }
+
+        public void DisplayPlaneInfo()
+        {
+            Console.WriteLine("Назва літака: " + planeName);
+            Console.WriteLine("Виробник: " + manufacturer);
+            Console.WriteLine("Рік випуску: " + yearOfManufacture);
+            Console.WriteLine("Тип літака: " + planeType);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Plane plane1 = new Plane("Boeing 737", "Boeing", 2010, "Пасажирський");
+
+        plane1.DisplayPlaneInfo();
+    }
+}
