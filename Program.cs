e6f601f36d3427f3cc0c2a44b436d8eb7e457430
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:00:17 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 2, 8, 1, 9 };
+
+        Console.WriteLine("Оберіть напрям сортування:");
+        Console.WriteLine("1. За зростанням");
+        Console.WriteLine("2. За зменшенням");
+        int choice = int.Parse(Console.ReadLine());
+
+        bool ascending = (choice == 1) ? true : false;
+
+        SortUtils.BubbleSort(array, ascending);
+
+        Console.WriteLine("Відсортований масив:");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ SortUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    public class SortUtils
+    {
+        public static void BubbleSort(int[] array, bool ascending)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if ((ascending && array[j] > array[j + 1]) || (!ascending && array[j] < array[j + 1]))
+                    {
+                         int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+}
