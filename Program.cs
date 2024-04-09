d731ff25269513d93e683d37d6648854b3e88b58
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:06:50 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sideLength = 5;
+        char symbol = '*';
+
+        SquareDrawer.DrawSquare(sideLength, symbol);
+    }
+}
---
+++ SquareDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    using System;
+
+    public class SquareDrawer
+    {
+        public static void DrawSquare(int sideLength, char symbol)
+        {
+            for (int i = 0; i < sideLength; i++)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {
+                    Console.Write(symbol + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
