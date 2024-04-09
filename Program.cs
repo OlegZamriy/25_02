1ca61cd3eabb7a1a3b01850c7690d7863b11ca1a
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:10:13 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Website website1 = new Website();
+
+        website1.SetWebsiteName("Example.com");
+        website1.SetWebsitePath("/home");
+        website1.SetWebsiteDescription("This is an example website");
+        website1.SetWebsiteIpAddress("192.168.1.1");
+
+        website1.DisplayWebsiteInfo();
+    }
+}
---
+++ Website.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    using System;
+
+    public class Website
+    {
+        private string websiteName;
+        private string websitePath;
+        private string websiteDescription;
+        private string websiteIpAddress;
+
+        public void SetWebsiteName(string name)
+        {
+            websiteName = name;
+        }
+
+        public string GetWebsiteName()
+        {
+            return websiteName;
+        }
+
+        public void SetWebsitePath(string path)
+        {
+            websitePath = path;
+        }
+
+        public string GetWebsitePath()
+        {
+            return websitePath;
+        }
+
+        public void SetWebsiteDescription(string description)
+        {
+            websiteDescription = description;
+        }
+
+        public string GetWebsiteDescription()
+        {
+            return websiteDescription;
+        }
+
+        public void SetWebsiteIpAddress(string ipAddress)
+        {
+            websiteIpAddress = ipAddress;
+        }
+
+        public string GetWebsiteIpAddress()
+        {
+            return websiteIpAddress;
+        }
+
+        public void DisplayWebsiteInfo()
+        {
+            Console.WriteLine("Назва сайту: " + websiteName);
+            Console.WriteLine("Шлях до сайту: " + websitePath);
+            Console.WriteLine("Опис сайту: " + websiteDescription);
+            Console.WriteLine("IP-адреса сайту: " + websiteIpAddress);
+        }
+    }
+
+}
