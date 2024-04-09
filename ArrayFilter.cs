d179c7812ac719558c770a7ae45f25e670f4f844
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:09:03 +0300

    1

---
+++ ArrayFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ArrayFilter
+    {
+        public static int[] FilterArray(int[] originalArray, int[] filterArray)
+        {
+            List<int> filteredList = new List<int>(originalArray);
+
+            foreach (int item in filterArray)
+            {
+                filteredList.RemoveAll(i => i == item);
+            }
+
+            return filteredList.ToArray();
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] originalArray = { 1, 2, 6, -1, 88, 7, 6 };
+        int[] filterArray = { 6, 88, 7 };
+
+        int[] resultArray = ArrayFilter.FilterArray(originalArray, filterArray);
+
+        Console.WriteLine("Результат фільтрації:");
+        foreach (int item in resultArray)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
