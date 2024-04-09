e79d37ca006711ddb8075f7276ece3dd2e69ffe3
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 19:59:03 +0300

    1

---
+++ MathUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    using System;
+
+    public class MathUtils
+    {
+        public static bool IsFibonacciAndPrime(int number)
+        {
+            if (IsFibonacci(number) && IsPrime(number))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsFibonacci(int number)
+        {
+            int a = 0, b = 1;
+
+            while (b < number)
+            {
+                int temp = b;
+                b = a + b;
+                a = temp;
+            }
+
+            return b == number;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 13;  
+
+        bool result = MathUtils.IsFibonacciAndPrime(number);
+
+        Console.WriteLine($"Число {number} є числом Фібоначчі та простим: {result}");
+    }
+}
