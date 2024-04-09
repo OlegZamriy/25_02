20458e5d37a543befbf72eb04f8679478ad48ab3
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:02:33 +0300

    1

---
+++ Employee.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    using System;
+
+    public class Employee
+    {
+        private string fullName;
+        private DateTime dateOfBirth;
+        private string contactPhone;
+        private string workEmail;
+        private string position;
+        private string jobDescription;
+
+        public Employee()
+        {
+            fullName = "";
+            dateOfBirth = DateTime.MinValue;
+            contactPhone = "";
+            workEmail = "";
+            position = "";
+            jobDescription = "";
+        }
+
+        public void SetFullName(string name)
+        {
+            fullName = name;
+        }
+
+        public string GetFullName()
+        {
+            return fullName;
+        }
+
+        public void SetDateOfBirth(DateTime dob)
+        {
+            dateOfBirth = dob;
+        }
+
+        public DateTime GetDateOfBirth()
+        {
+            return dateOfBirth;
+        }
+
+        public void SetContactPhone(string phone)
+        {
+            contactPhone = phone;
+        }
+
+        public string GetContactPhone()
+        {
+            return contactPhone;
+        }
+
+        public void SetWorkEmail(string email)
+        {
+            workEmail = email;
+        }
+
+        public string GetWorkEmail()
+        {
+            return workEmail;
+        }
+
+        public void SetPosition(string pos)
+        {
+            position = pos;
+        }
+
+        public string GetPosition()
+        {
+            return position;
+        }
+
+        public void SetJobDescription(string desc)
+        {
+            jobDescription = desc;
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine("ПІБ: " + fullName);
+            Console.WriteLine("Дата народження: " + dateOfBirth.ToString("dd.MM.yyyy"));
+            Console.WriteLine("Контактний телефон: " + contactPhone);
+            Console.WriteLine("Робочий email: " + workEmail);
+            Console.WriteLine("Посада: " + position);
+            Console.WriteLine("Обов'язки:");
+            Console.WriteLine(jobDescription);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Employee employee1 = new Employee();
+
+        employee1.SetFullName("Іванов Іван Іванович");
+        employee1.SetDateOfBirth(new DateTime(1985, 10, 15));
+        employee1.SetContactPhone("+380971234567");
+        employee1.SetWorkEmail("ivanov@example.com");
+        employee1.SetPosition("Програміст");
+        employee1.SetJobDescription("Розробка програмного забезпечення.");
+
+        employee1.DisplayEmployeeInfo();
+    }
+}
