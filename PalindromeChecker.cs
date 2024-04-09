73416f78f53f88eec8582ef05e2a870d31a62690
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:07:59 +0300

    1

---
+++ PalindromeChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    public class PalindromeChecker
+    {
+        public static bool IsPalindrome(int number)
+        {
+            int reverse = 0;
+            int original = number;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                reverse = reverse * 10 + digit;
+                number /= 10;
+            }
+
+            return original == reverse;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 1221;
+        int num2 = 3443;
+        int num3 = 7854;
+
+        Console.WriteLine($"{num1} - паліндром? {PalindromeChecker.IsPalindrome(num1)}");
+        Console.WriteLine($"{num2} - паліндром? {PalindromeChecker.IsPalindrome(num2)}");
+        Console.WriteLine($"{num3} - паліндром? {PalindromeChecker.IsPalindrome(num3)}");
+    }
+}
