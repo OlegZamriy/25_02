1124418bf11e3a519d344eeb92a7345b1881001a
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:11:20 +0300

    1

---
+++ Journal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    using System;
+
+    public class Journal
+    {
+        private string journalName;
+        private int yearFounded;
+        private string journalDescription;
+        private string contactPhone;
+        private string email;
+
+        public void SetJournalName(string name)
+        {
+            journalName = name;
+        }
+
+        public string GetJournalName()
+        {
+            return journalName;
+        }
+
+        public void SetYearFounded(int year)
+        {
+            yearFounded = year;
+        }
+
+        public int GetYearFounded()
+        {
+            return yearFounded;
+        }
+
+        public void SetJournalDescription(string description)
+        {
+            journalDescription = description;
+        }
+
+        public string GetJournalDescription()
+        {
+            return journalDescription;
+        }
+
+        public void SetContactPhone(string phone)
+        {
+            contactPhone = phone;
+        }
+
+        public string GetContactPhone()
+        {
+            return contactPhone;
+        }
+
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public void DisplayJournalInfo()
+        {
+            Console.WriteLine("Назва журналу: " + journalName);
+            Console.WriteLine("Рік заснування: " + yearFounded);
+            Console.WriteLine("Опис журналу: " + journalDescription);
+            Console.WriteLine("Контактний телефон: " + contactPhone);
+            Console.WriteLine("Email: " + email);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Journal journal1 = new Journal();
+
+        journal1.SetJournalName("Science Journal");
+        journal1.SetYearFounded(2000);
+        journal1.SetJournalDescription("A journal for scientific research articles.");
+        journal1.SetContactPhone("+1234567890");
+        journal1.SetEmail("science@example.com");
+
+        journal1.DisplayJournalInfo();
+    }
+}
