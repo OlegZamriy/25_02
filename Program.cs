8dbc195ddcc806aeefb58a4a0f313d8f346eb38e
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 20:12:22 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using _25_02;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Store store1 = new Store();
+
+        store1.SetStoreName("Магазин Товари для дому");
+        store1.SetAddress("вул. Центральна, 123");
+        store1.SetStoreProfile("Продаж побутової техніки та меблів");
+        store1.SetContactPhone("+1234567890");
+        store1.SetEmail("store@example.com");
+
+        store1.DisplayStoreInfo();
+    }
+}
---
+++ Store.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_02
+{
+    using System;
+
+    public class Store
+    {
+        private string storeName;
+        private string address;
+        private string storeProfile;
+        private string contactPhone;
+        private string email;
+
+        public void SetStoreName(string name)
+        {
+            storeName = name;
+        }
+
+        public string GetStoreName()
+        {
+            return storeName;
+        }
+
+        public void SetAddress(string address)
+        {
+            this.address = address;
+        }
+
+        public string GetAddress()
+        {
+            return address;
+        }
+
+        public void SetStoreProfile(string profile)
+        {
+            storeProfile = profile;
+        }
+
+        public string GetStoreProfile()
+        {
+            return storeProfile;
+        }
+
+        public void SetContactPhone(string phone)
+        {
+            contactPhone = phone;
+        }
+
+        public string GetContactPhone()
+        {
+            return contactPhone;
+        }
+
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public void DisplayStoreInfo()
+        {
+            Console.WriteLine("Назва магазину: " + storeName);
+            Console.WriteLine("Адреса: " + address);
+            Console.WriteLine("Опис профілю магазину: " + storeProfile);
+            Console.WriteLine("Контактний телефон: " + contactPhone);
+            Console.WriteLine("Email: " + email);
+        }
+    }
+
+}
